7be04f020c96877da4f50cc2eb4a5cbcb8a340bc
Hui-Hh77 <110591@mail2.chshs.ntpc.edu.tw>
2025-04-07 09:52:36 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
-import Projects from "@/components/Projects";
 import TabInterface from "@/components/TabInterface";
 import Contest from "@/components/Contest";
 import BlogSection from "@/components/BlogSection";
-import SocialLink from "@/components/SocialLink";
 import Radar from "@/components/Radar";
 
 export default function Home() {
@@ -16,9 +14,7 @@ export default function Home() {
         <Radar />
         <TabInterface />
         <Contest />
-        <Projects />
         <BlogSection limit={5} />
-        <SocialLink />
       </div>
     </main>
   );
